63f52d49b84a594a15b4cdf991fba04a6cc1e940
Vinicius Romani <vn.romani@gmail.com>
2024-01-01 09:32:21 -0300

    basic folder structure

---
+++ app/index.ts
@@ -0,0 +1,17 @@
+require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+
+const app = express();
+
+app.use(cors({
+  credentials: true,
+}));
+app.use(express.json());
+
+const server = http.createServer(app);
+
+server.listen(8000, () => {
+  console.log('server running on localhost:8000')
+});
